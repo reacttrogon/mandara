b5ec8486370fd2a3bd68834a1a74120d0909df4e
abinandd <112941816+abinandd@users.noreply.github.com>
2026-01-14 12:06:08 +0530

    added favicon , added seo contents

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { title } from "framer-motion/client";
 import "./globals.css";
 import { DM_Sans } from "next/font/google";
 
@@ -6,6 +7,35 @@ const dmSans = DM_Sans({
   variable: "--font-dm-sans",
   weight: ["400", "500", "700"],
 });
+
+export const metaData = {
+  title: "Mandara Wellness Clinic",
+  description:
+    "Mandara Wellness Clinic is a premier postnatal care facility where medical excellence meets luxury hospitality. We provide new mothers and their babies with an unparalleled experience of comfort, care, and holistic recovery.",
+  keywords: [
+    "Postnatal care",
+    "Wellness clinic",
+    "Luxury postnatal recovery",
+    "New mother care",
+    "Holistic recovery clinic",
+    "Mandara Wellness",
+  ],
+   openGraph: {
+    title: "Mandara Wellness Clinic",
+    description: "Mandara Wellness Clinic is a premier postnatal care facility where medical excellence meets luxury hospitality. We provide new mothers and their babies with an unparalleled experience of comfort, care, and holistic recovery.",
+    url: "https://mandara-web.trogon.info/",
+    siteName: "Mandara Wellness",
+    images: [
+      {
+        url: "/assets/images/image1.webp", 
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (

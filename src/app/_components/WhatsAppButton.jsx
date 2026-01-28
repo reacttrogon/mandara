ced0a792db7d1eb6e0018c71e2cda47359239942
abhinand <abhinand155@gmail.com>
2026-01-28 16:15:07 +0530

    changed whatsapp icon number

--- src/app/_components/WhatsAppButton.jsx
+++ src/app/_components/WhatsAppButton.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function WhatsAppButton() {
     return (
         <a
-            href="https://wa.me/917654389716"
+            href="https://wa.me/919961200055"
             target="_blank"
             rel="noopener noreferrer"
             className="fixed bottom-6 right-6 z-50 flex items-center justify-center w-14 h-14 bg-[#25D366] rounded-full shadow-lg hover:bg-[#20bd5a] transition-all duration-300 hover:scale-110 active:scale-95 group"

a4f0ac875d470251532914991513d485589cdaeb
abhinand <abhinand155@gmail.com>
2026-01-29 11:57:39 +0530

    removed unwanted components

--- src/app/page.js
+++ src/app/page.js
@@ -3,12 +3,8 @@
 import Header from "./_components/Header.jsx";
 import HeroSlider from "./_components/HeroSlider.jsx";
 import AboutMandara from "./_components/AboutMandara";
-import Services from "./_components/Services";
-import WhatWeOfferSection from "./_components/WhatWeOfferSection.jsx";
-import WhyChooseUsSection from "./_components/WhyChooseUsSection.jsx";
 import GallerySection from "./_components/GallerySection.jsx";
 import FAQSection from "./_components/FAQSection.jsx";
-import AmenitiesSection from "./_components/AmenitiesSection.jsx";
 import WhoShouldJoinSection from "./_components/WhoShouldJoinSection.jsx";
 import ContactUs from "./_components/ContactUs.jsx";
 import Footer from "./_components/Footer.jsx";
@@ -24,12 +20,8 @@ export default function Home() {
       <AboutMandara />
       <Philosophy />
       <VideoScrollSection />
-      {/* <Services />
-      <WhatWeOfferSection />
-      <WhyChooseUsSection /> */}
       <GallerySection />
       <FAQSection />
-      {/* <AmenitiesSection /> */}
       <WhoShouldJoinSection />
       <ContactUs />
       <Footer />

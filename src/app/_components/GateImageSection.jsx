5562eb0ef7f28412b6a62abe1dd7fedc3852a298
abhinand <abhinand155@gmail.com>
2026-01-31 11:40:44 +0530

    image change

--- src/app/_components/GateImageSection.jsx
+++ src/app/_components/GateImageSection.jsx
@@ -6,8 +6,8 @@ export default function GateImageSection() {
             <div className="container mx-auto px-4 lg:px-8">
                 <div className="relative w-full max-w-[80vw] lg:max-w-[70vw] mx-auto aspect-video rounded-2xl overflow-hidden shadow-2xl ring-1 ring-dark/5 group cursor-pointer">
                     <Image
-                        src="/assets/images/frontview.jpeg"
-                        //  src="/assets/images/frontview2.webp"
+                        // src="/assets/images/frontview.jpeg"
+                         src="/assets/images/frontview2.webp"
                         alt="Mandara Gate"
                         className="w-full h-full object-cover"
                         fill
